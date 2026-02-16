69fd85cc882410478b8775724220f0cce8bc8260
Dioneibi-rip <fandubdreams@gmail.com>
2026-02-15 23:18:56 -0400

    Actualizar object-repository.d.ts

--- lib/Store/object-repository.d.ts
+++ lib/Store/object-repository.d.ts
@@ -1,14 +1,10 @@
-import { Store } from 'cache-manager'
-import { AuthenticationCreds } from '../Types'
-
-export declare const makeCacheManagerAuthState: (store: Store, sessionKey: string) => Promise<{
-    clearState: () => Promise<void>
-    saveCreds: () => Promise<void>
-    state: {
-        creds: AuthenticationCreds
-        keys: {
-            get: (type: string, ids: string[]) => Promise<{}>
-            set: (data: any) => Promise<void>
-        }
-    }
-}>
+export declare class ObjectRepository<T extends object> {
+    readonly entityMap: Map<string, T>
+    constructor(entities?: Record<string, T>)
+    findById(id: string): T | undefined
+    findAll(): T[]
+    upsertById(id: string, entity: T): Map<string, T>
+    deleteById(id: string): boolean
+    count(): number
+    toJSON(): T[]
+}
