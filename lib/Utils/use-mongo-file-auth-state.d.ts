81dc167cbf8f7a2145ee80614845287f28d68b2d
Dioneibi-rip <fandubdreams@gmail.com>
2026-02-21 12:46:36 -0400

    improve LID/JID normalization and group admin compatibility

--- lib/Utils/use-mongo-file-auth-state.d.ts
+++ lib/Utils/use-mongo-file-auth-state.d.ts
@@ -1,6 +1,11 @@
 import { AuthenticationState } from '../Types'
 
-export declare const useMongoFileAuthState: (collection: string) => Promise<{
+export declare const useMongoFileAuthState: (collection: {
+    findOne: (filter: { _id: string }) => Promise<unknown>
+    updateOne: (filter: { _id: string }, update: { $set: Record<string, unknown> }, options: { upsert: boolean }) => Promise<unknown>
+    deleteOne: (filter: { _id: string }) => Promise<unknown>
+    bulkWrite?: (operations: Array<Record<string, unknown>>, options: { ordered: boolean }) => Promise<unknown>
+}) => Promise<{
     state: AuthenticationState
     saveCreds: () => Promise<void>
 }>
